db37e0476a2159cfa6d197d2c185397652f8bd60
rmangra <richard.mangra@gmail.com>
2018-01-30 17:00:32 -0500

    Added new View for Cryptocurrency data table.

--- CryptoWebscraperMVC/CryptoWebscraperMVC/App_Start/RouteConfig.cs
+++ CryptoWebscraperMVC/CryptoWebscraperMVC/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace CryptoWebscraperMVC
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "CryptoCurrency", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
---
+++ CryptoWebscraperMVC/CryptoWebscraperMVC/Controllers/CryptoCurrencyController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CryptoWebscraperMVC.Controllers
+{
+    public class CryptoCurrencyController : Controller
+    {
+        // GET: CryptoCurrency
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
